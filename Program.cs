2d2c8ac0d3fad5f5e227dd30a24840a01871a1da
Varia S <especificita@gmail.com>
2022-07-15 17:02:55 +0300

    Replaced int with double in 43

--- Program.cs
+++ Program.cs
@@ -18,15 +18,15 @@ void Task43()
 {
 Console.WriteLine("Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
 Console.WriteLine("Введите значение b1");
-int b1 = Convert.ToInt32 (Console.ReadLine ());
+double b1 = Convert.ToDouble (Console.ReadLine ());
 Console.WriteLine("Введите значение k1");
-int k1 = Convert.ToInt32 (Console.ReadLine ());
+double k1 = Convert.ToDouble (Console.ReadLine ());
 Console.WriteLine("Введите значение b2");
-int b2 = Convert.ToInt32 (Console.ReadLine ());
+double b2 = Convert.ToDouble (Console.ReadLine ());
 Console.WriteLine("Введите значение k2");
-int k2 = Convert.ToInt32 (Console.ReadLine ());
-int x = (b2 - b1)/(k1 - k2);
-int y = k1*((b2 - b1)/(k1 - k2)) + b1;
+double k2 = Convert.ToInt32 (Console.ReadLine ());
+double x = (b2 - b1)/(k1 - k2);
+double y = k1*x + b1;
 Console.WriteLine($"Координаты пересечения двух прямых: ({x}, {y})");
 }
 Task43();
